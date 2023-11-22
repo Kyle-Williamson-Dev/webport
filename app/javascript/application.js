92a1fc0eb10cc4fc92b4bc79e136925ef65ff745
Kyle Williamson <kwilliamson0232@gmail.com>
2023-11-22 01:22:52 -0700

    Added nav media query and JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+const burger = document.querySelector(".burger");
+const navList = document.querySelector(".navlist");
+
+burger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  burger.classList.toggle("active");
+  navList.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll("nav-link");
